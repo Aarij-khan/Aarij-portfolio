c9a545856566ef79329782a563019d1a9b94fd54
Aarij khan <aarij0100@gmail.com>
2025-09-23 21:25:30 +0500

    update

--- src/Pages/Project/ProjectBannerData.js
+++ src/Pages/Project/ProjectBannerData.js
@@ -1,16 +1,43 @@
 const ProjectBannerData = [
     {
-        imgUrl: "./assets/images/zal.png",
+        imgUrl: "./assets/images/grocery.png",
         serialNo: "01",
+        heading: "Grocery website project",
+        para: "I have completed my latest project on a Grocery Store using HTML, CSS, and JavaScript. We can also use the Next.js framework for better performance, but this project will be used in Shopify and WordPress themes.",
+        read: "./assets/images/Read-More.png",
+        readLink: "https://grocery-web-by-aarij.netlify.app/",
+        isReverse: true
+    },
+    {
+        imgUrl: "./assets/images/fabrin.png",
+        serialNo: "02",
+        heading: "Fabrin website project",
+        para: "I have completed my latest project on a Fabrin website using HTML, CSS, and JavaScript. We can also use the Next.js framework for better performance, but this project will be used in Shopify and WordPress themes.",
+        read: "./assets/images/Read-More.png",
+        readLink: "https://fabrin-web-by-aarij.netlify.app/",
+        isReverse: false
+    },
+    {
+        imgUrl: "./assets/images/zal.png",
+        serialNo: "03",
         heading: "Mern Stack E-commerce project",
         para: "I have completed my latest project on e-commerce selling product using Next js and Mongodb, This Website have all functionality like Roles management, Product management, Order management, User management, Payment gateway integration, and many more. This project is a full-stack application that allows users to browse products, add them to their cart, and make purchases securely.",
         read: "./assets/images/Read-More.png",
         readLink: "https://zalvox.vercel.app",
+        isReverse: true
+    },
+    {
+        imgUrl: "./assets/images/prestige.png",
+        serialNo: "04",
+        heading: "Prestigedials project",
+        para: "I have completed my latest project on Prestigedials using Next.js and MongoDB. This website has all the functionality like Roles management, Product management, Order management, User management, Payment gateway integration, and many more. This project is a full-stack application that allows users to browse products, add them to their cart, and make purchases securely.",
+        read: "./assets/images/Read-More.png",
+        readLink: "https://www.prestigedials.shop/",
         isReverse: false
     },
     {
         imgUrl: "./assets/images/webcanva.png",
-        serialNo: "02",
+        serialNo: "05",
         heading: "Saas Landing Page (Web canva studio)",
         para: "I have completed my latest project on Saas Landing Page for Webcanva, This project is a fully responsive and animated website built using Next.js. It showcases the features and benefits of Webcanva, a platform for creating stunning graphics and designs. The site is designed to provide an engaging user experience with smooth animations and a modern layout.",
         read: "./assets/images/Read-More.png",
@@ -19,7 +46,7 @@ const ProjectBannerData = [
     },
     {
         imgUrl: "./assets/images/eapp.png",
-        serialNo: "03",
+        serialNo: "06",
         heading: "E-commerce Delivery App",
         para: "I have completed my latest project on e-commerce app using React Native Expo, This app comes with all the core features you’d expect. Smooth navigation and a clean, modern design. Shopping cart and wishlist functionality. User authentication powered by Firebase. A simple and efficient backend with Firebase Firestore + authentication.",
         read: "./assets/images/Read-More.png",
@@ -28,7 +55,7 @@ const ProjectBannerData = [
     },
     {
         imgUrl: "./assets/images/food.png",
-        serialNo: "04",
+        serialNo: "07",
         heading: "Food delivery app (React Native)",
         para: "I have completed this food delivery app using React Native Expo, This app comes with all the core features you’d expect. Smooth navigation and a clean, modern design. Shopping cart and wishlist functionality & also have a dashboard for the admin to manage their inventory and orders.",
         read: "./assets/images/Read-More.png",
@@ -37,7 +64,7 @@ const ProjectBannerData = [
     },
     {
         imgUrl: "./assets/images/fitness.png",
-        serialNo: "05",
+        serialNo: "08",
         heading: "Fitness App",
         para: "I developed a visually engaging and interactive Fitness App using React Native Expo, designed to enhance the user experience through smooth animations and intuitive navigation.",
         read: "./assets/images/Read-More.png",
@@ -46,7 +73,7 @@ const ProjectBannerData = [
     },
     {
         imgUrl: "./assets/images/bloodbankapp.jpg",
-        serialNo: "06",
+        serialNo: "09",
         heading: "Blood Bank App (Hackathon Project)",
         para: "I developed a Blood Bank app using React Native and Firebase. This mobile application connects users with healthcare professionals and allows individuals in need of blood to reach out to potential donors. Users can browse doctor profiles, register as donors, and engage in real-time chat for seamless communication. With Firebase managing the backend, the app ensures secure data handling and instant interactions. This project not only enhanced my skills in mobile app development but also allowed me to contribute to a meaningful cause. I’m proud to showcase the Blood Bank app as part of my portfolio!",
         read: "./assets/images/Read-More.png",
@@ -55,7 +82,7 @@ const ProjectBannerData = [
     },
     {
         imgUrl: "./assets/images/image.jfif",
-        serialNo: "07",
+        serialNo: "10",
         heading: "Saas landing page",
         para: "🚀 Frontend development :) Successfully developed a modern, fully responsive, and animated website using Next.js. Focused on building high-performance, user-friendly, and visually engaging web experiences.",
         read: "./assets/images/Read-More.png",
@@ -64,7 +91,7 @@ const ProjectBannerData = [
     },
     {
         imgUrl: "./assets/images/bar.png",
-        serialNo: "08",
+        serialNo: "11",
         heading: "Barber appointment Website",
         para: "I created a Barber Website that allows users to book appointments with barbers easily. The site features a user-friendly interface where clients can select their preferred barber and schedule an appointment. Barbers can also promote their services by posting ads through a dedicated dashboard. I built this project using HTML, CSS, and JavaScript, with Firebase handling the backend for secure data management and real-time updates. This project not only sharpened my web development skills but also taught me about creating effective user interfaces. I’m excited to feature the Barber Website in my portfolio!",
         read: "./assets/images/Read-More.png",
@@ -73,7 +100,7 @@ const ProjectBannerData = [
     },
     {
         imgUrl: "./assets/images/solar.png",
-        serialNo: "09",
+        serialNo: "12",
         heading: "Solar panel website",
         para: "I developed a Solar Panel website that provides users with information about solar energy solutions. The site features a user-friendly interface where visitors can explore various solar panel options and their benefits. I built this project using React js and some animation libraries, ensuring a responsive design for optimal viewing on all devices. This project not only enhanced my web development skills but also deepened my understanding of renewable energy solutions. I’m excited to showcase the Solar Panel website in my portfolio!",
         read: "./assets/images/Read-More.png",
